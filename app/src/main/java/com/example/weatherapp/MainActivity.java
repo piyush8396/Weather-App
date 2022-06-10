6a2e8c97d8ae67ca7caf70d8778a4668459bfe4d
piyush <ayush787jaiswal@gmail.com>
2022-06-10 17:51:23 +0530

    Pojo for Retrofit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -16,60 +17,188 @@ import android.location.Location;
 
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationRequest;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.text.ParcelableSpan;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity implements LocationListener {
-    LocationManager locationManager;
-String city="ram";
+    String APPId = "1aa1c77170fcdb824e74e94c02bd799b";
+
+    //  static FusedLocationProviderClient fusedLocationProvideClient;
+    TextView temp;
+    TextView city;
+    TextView day1;
+    TextView temp1;
+    TextView day2;
+    TextView temp2;
+    TextView day3;
+    TextView temp3;
+    TextView day4;
+    TextView temp4;
+    TextView day5;
+    TextView temp5;
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         temp=(TextView) findViewById(R.id.temp) ;
+         city=(TextView) findViewById(R.id.city_name);
+        day1 = (TextView) findViewById(R.id.day1);
+        temp1 = (TextView) findViewById(R.id.temp1);
+        day2 = (TextView) findViewById(R.id.day2);
+        temp2 = (TextView) findViewById(R.id.temp2);
+        day3 = (TextView) findViewById(R.id.day3);
+        temp3 = (TextView) findViewById(R.id.temp3);
+        day4 = (TextView) findViewById(R.id.day4);
+        temp4 = (TextView) findViewById(R.id.temp4);
+        day5 = (TextView) findViewById(R.id.day5);
+        temp5 = (TextView) findViewById(R.id.temp5);
 
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    100);
+
         }
-        getLocation();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 1, this);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 1, this);
+        Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location!=null) {
+            Log.d("lat1", Double.toString(location.getLatitude()));
+            Log.d("lat2", Double.toString(location.getLongitude()));
+            APiInterface aPiInterface = RetroFitClient.getRetrofit().create(APiInterface.class);
+            Call<ModelClass> call = aPiInterface.getCurrentWeatherData(Double.toString(location.getLatitude()),
+                    Double.toString(location.getLongitude()), "20", APPId);
+            call.enqueue(new Callback<ModelClass>() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onResponse(Call<ModelClass> call, Response<ModelClass> response) {
+                    Log.d("ram1","aa gya");
+                    if (response.code() == 200) {
+                        Log.d("ram","aa gya");
 
-    }
+                        ModelClass modelClass = response.body();
+                       // assert modelClass != null;
+                        Log.d("COD",modelClass.cod);
+                        Log.d("size",Integer.toString(modelClass.list.size()));
+                        for (int i = 0; i < modelClass.list.size(); i++) {
+                            if (i == 0) {
+                                Log.d("test",Double.toString(modelClass.list.get(i).main.temp));
+                            temp.setText(Double.toString(modelClass.list.get(i).main.temp));
+                            temp1.setText(Double.toString(modelClass.list.get(i).main.temp));
+                               city.setText(modelClass.city.name);
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    Date date = format.parse(modelClass.list.get(i).dt_txt);
+                                    day1.setText((String) DateFormat.format("EEEE", date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
 
-    @SuppressLint("MissingPermission")
-    private void getLocation() {
-        try {
-            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-           locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5, MainActivity.this);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, MainActivity.this);
-         //   requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-         //   requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-           // Log.d("city_name",city);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+                            }
+                            if (i == 4) {
+                                temp2.setText(Double.toString(modelClass.list.get(i).main.temp));
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    Date date = format.parse(modelClass.list.get(i).dt_txt);
+                                    day2.setText((String) DateFormat.format("EEEE", date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (i == 8) {
+                                temp3.setText(Double.toString(modelClass.list.get(i).main.temp));
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    Date date = format.parse(modelClass.list.get(i).dt_txt);
+                                    day3.setText((String) DateFormat.format("EEEE", date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (i == 12) {
+                                temp4.setText(Double.toString(modelClass.list.get(i).main.temp));
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    Date date = format.parse(modelClass.list.get(i).dt_txt);
+                                    day4.setText((String) DateFormat.format("EEEE", date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (i == 19) {
+                                temp5.setText(Double.toString(modelClass.list.get(i).main.temp));
+                                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                try {
+                                    Date date = format.parse(modelClass.list.get(i).dt_txt);
+                                    day5.setText((String) DateFormat.format("EEEE", date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ModelClass> call, Throwable t) {
+                }
+            });
         }
-
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        Log.d("city_name1",city);
-        try {
-            Geocoder geocoder=new Geocoder(MainActivity.this, Locale.getDefault());
-            List<Address> addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-            city=addresses.get(0).getAddressLine(0);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        location.getLongitude();
+        location.getLatitude();
 
     }
 
     @Override
-    public void onLocationChanged(@NonNull List<Location> locations) {
-        LocationListener.super.onLocationChanged(locations);
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
     }
 
     @Override
@@ -81,5 +210,8 @@ String city="ram";
     public void onProviderDisabled(@NonNull String provider) {
         LocationListener.super.onProviderDisabled(provider);
     }
-   // Log.d("city_name",city);
+
+
+
+
 }
